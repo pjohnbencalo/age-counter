6b8c61dff5d2ade54db60570c0af2460e4282e24
Philip John Bencalo <pj.bencalo@gmail.com>
2023-05-26 20:44:56 +0800

    Change moment to luxon

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 document.querySelector("form").addEventListener("submit", function (e) {
   e.preventDefault(); // Prevent form submission
-  let selectedDate = document.getElementById("date").value;
-  // Add the moment.js
-  let momentAge = moment(selectedDate);
-  let calculatedAge = momentAge.fromNow();
+  // Add the luxon.js
+  const DateTime = luxon.DateTime;
+  let selectedDate = DateTime.fromISO(document.getElementById("date").value);
+  const now = DateTime.now();
+  const calculatedAge = selectedDate.toRelative({ base: now });
   document.getElementById("age").innerHTML = calculatedAge;
 });
